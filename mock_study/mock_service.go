e60497fe4b6f6279dcf1dee1bad99445a71b73c7
treemakerli <874188225@qq.com>
2022-07-14 18:49:44 +0800

    go mock study

--- mock_study/mock_service.go
+++ mock_study/mock_service.go
@@ -1,6 +1,7 @@
 // Package mock_study show my study of go mock.
 package mock_study
 
+// mockgen -destination:"mockgen生成的文件存放的位置以及文件的名字" -package:"mocks" modules/package interface
 type MockService interface {
 	MethodOne(i int) (string, error)
 }
--- tester/mock_study/mock_service_test.go
+++ tester/mock_study/mock_service_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-var i int = 1
+var req int = 1
 
 func TestMockService(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -15,7 +15,7 @@ func TestMockService(t *testing.T) {
 	mockService := mocks.NewMockMockService(controller)
 	mockService.EXPECT().MethodOne(1).Return("a", nil)
 	//mockService.EXPECT().MethodOne(2).Return("b", errors.New("bbb"))
-	if resp, err := mockService.MethodOne(i); err != nil {
+	if resp, err := mockService.MethodOne(req); err != nil {
 		t.Fatalf("1: %v, %v\n", resp, err)
 	}
 }
